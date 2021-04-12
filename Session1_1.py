bd3438f90f8707749bada3e7f63256c7ab7a1cf8
Eva Szatmari <szateva@gmail.com>
2021-04-12 12:24:37 +0100

    session 1 solved

--- Session1_1.py
+++ Session1_1.py
@@ -17,3 +17,13 @@ TESTS:
 Input: 2, 3, 6 Output: 11
 Input: 0, 20, 300 Output: 320
 Input: -5, 180, -17 Output: 158 """
+
+num1 = float(input('number 1 '))
+
+num2 = float(input('number 2 '))
+
+num3 = float(input('number 3 '))
+
+total = num1 + num2 + num3
+
+print(total)
