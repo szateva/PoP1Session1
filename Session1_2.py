9dd6978fca152c822f44730e5165a9866004b17c
Eva Szatmari <szateva@gmail.com>
2021-04-12 12:25:46 +0100

    session 1_2 file added

---
+++ Session1_2.py
@@ -0,0 +1,19 @@
+# Session 1 Problem 02: Greeting
+
+""" STATEMENT: Write a program that greets the user by printing the word
+"Hello", a comma, the name of the user and an exclamation mark after it.
+See the examples below.
+
+Warning. Your program's output should strictly match the desired one,
+character by character. There shouldn't be any space between the name
+and the exclamation mark.
+
+
+For example, on input John Doe outputs should be Hello, John Doe!
+
+TEMPLATE:
+
+TESTS:
+
+Input: Harry Output: Hello, Harry!
+Input: Mr. Jack Sparrow Output: Hello, Mr. Jack Sparrow! """
