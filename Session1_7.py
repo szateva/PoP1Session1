b77bbb18d5cb5d83d55e97b68b05d416897f2140
Eva Szatmari <szateva@gmail.com>
2021-04-12 16:30:49 +0100

    Session1_7 is solved

--- Session1_7.py
+++ Session1_7.py
@@ -10,3 +10,13 @@ Input: 5, 6 Output: 5
 Input: 0, 10 Output: 0
 Input: 3, 3 Output: 3
 Input: -5, -10 Output: -10 """
+
+num1 = int(input("What is the first number? "))
+num2 = int(input("What is the second number? "))
+
+if num1 <= num2:
+    min = num1
+else:
+    min = num2
+
+print("The minimum of the two numbers is ", min)
