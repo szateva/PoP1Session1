0a4258cea5087119f1c8cc1f7b6c5f7b0cbd77ef
Eva Szatmari <szateva@gmail.com>
2021-04-12 12:28:52 +0100

    session 1_3 added

---
+++ Session1_3.py
@@ -0,0 +1,17 @@
+# Session 1 Problem 03: Sharing
+
+""" STATEMENT:
+N students take K pens and distribute them among each other evenly.
+The remaining (the undivisible) part remains in the box.
+How many pens will each single student get?
+How many pens will remain in the box?
+The program reads the numbers N and K.
+It should print the two answers for the questions above.
+
+For example, on input 5, 17 output should be 3, 2
+TEMPLATE:
+
+TESTS:
+Input: 6, 50 Output: 8, 2
+Input: 1, 10 Output: 10, 0
+Input: 4, 2 Output: 0, 2 """
