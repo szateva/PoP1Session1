2004386e27c479b1b3b028ae227d359f8f24e33d
Eva Szatmari <szateva@gmail.com>
2021-04-12 23:47:01 +0100

    Session1_8 is solved

--- Session1_8.py
+++ Session1_8.py
@@ -11,3 +11,16 @@ Input: 7, 3, 4 Output: 3
 Input: 4, 7, 0 Output: 0
 Input: 4, 4, 2 Output: 2
 Input: 7, 3, 3 Output: 3 """
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+num3 = int(input("Third number: "))
+
+if num1 <= num2 and num1 <= num3:
+    min = num1
+elif num2 <= num1 and num2 <= num3:
+    min = num2
+else:
+    min = num3
+
+print("The minimum of the three numbers is:", min)
