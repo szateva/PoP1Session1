ff28cc302e752194bd0f6ba5e18f6a8f1d0352c6
Eva Szatmari <szateva@gmail.com>
2021-04-12 16:26:17 +0100

    Session1_6 is solved

--- Session1_6.py
+++ Session1_6.py
@@ -14,3 +14,16 @@ Input: 2, 50, 4 Output: 10, 0
 Input: 10, 15, 2 Output: 20, 30
 Input: 905, 79, 496 Output: 449271, 84
 Input: 1886, 73, 295 Output: 556585, 35 """
+
+pounds_per_flower = int(input("How many pounds per flower? "))
+pence_per_flower = int(input("How many pence per flower? "))
+no_of_flowers = int(input("How many flowers? "))
+
+cost_of_flower_in_pence = pounds_per_flower * 100 + pence_per_flower
+total_cost = cost_of_flower_in_pence * no_of_flowers
+
+total_pounds = total_cost // 100
+total_pence = total_cost % 100
+
+print("Total pounds to pay is ", total_pounds)
+print("Total pence to pay is ", total_pence)
