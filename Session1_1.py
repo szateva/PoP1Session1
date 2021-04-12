332dc3b1b036aa0cca314e3e212b437dd9f96183
Eva Szatmari <szateva@gmail.com>
2021-04-12 12:22:33 +0100

    first file

---
+++ Session1_1.py
@@ -0,0 +1,19 @@
+# Session 1 Problem 01: Sum of 3 numbers
+
+""" STATEMENT: Write a program that takes three numbers
+and prints their sum. Every number is given on a separate line.
+For example, on input 5, 6, 7 output should be 18
+
+TEMPLATE:
+
+num1 = float(input('number 1'))
+
+num2 = float(input('number 2'))
+
+num3 = float(input('number 3'))
+
+TESTS:
+
+Input: 2, 3, 6 Output: 11
+Input: 0, 20, 300 Output: 320
+Input: -5, 180, -17 Output: 158 """
