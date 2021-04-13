8129ca6fac7165758e7d1e79e432421390308741
Eva Szatmari <szateva@gmail.com>
2021-04-13 15:51:37 +0100

    Session1Extra1 is solved

--- Session1Extra1.py
+++ Session1Extra1.py
@@ -12,3 +12,11 @@ Input: 5 Output: 1
 Input: -15 Output: -1
 Input: 0 Output: 0
 Input: 1234 Output: 1 """
+
+num = int(input("Enter a number: "))
+if num == 0:
+    print("0")
+elif num < 0:
+    print("-1")
+else:
+    print("1")
