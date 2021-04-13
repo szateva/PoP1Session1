bdb2922c81d3c4e4442366a15675dc0ef1d28c3c
Eva Szatmari <szateva@gmail.com>
2021-04-13 15:58:21 +0100

    Session1Extra3 is solved

--- Session1Extra3.py
+++ Session1Extra3.py
@@ -14,3 +14,14 @@ Input: 6, 6, 1 Output: 2
 Input: 5, 9, 10 Output: 0
 Input: 3, 3, 8 Output: 2
 Input: 2, 9, 9 Output: 2 """
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+if num1 == num2 == num3:
+    print("3")
+elif num1 == num2 or num2 == num3 or num1 == num3:
+    print("2")
+else:
+    print("0")
