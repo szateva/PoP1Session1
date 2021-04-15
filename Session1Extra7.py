23433d71947b128d23b3cdb90970b082362793fe
Eva Szatmari <szateva@gmail.com>
2021-04-15 10:46:00 +0100

    Session1Extra7 solved

--- Session1Extra7.py
+++ Session1Extra7.py
@@ -11,3 +11,11 @@ Input: 190 Output: 120
 Input: 5 Output: 60
 Input: 29 Output: 348
 Input: 192 Output: 144 """
+
+hour_hand_degree = float(input("How many degrees had the hour hand moved? "))
+minute_hand_degree = hour_hand_degree * 12
+
+hours_passed = minute_hand_degree // 360
+current_hour_degree = hours_passed * 30
+current_minute_degree = int(minute_hand_degree % 360)
+print(current_minute_degree)
