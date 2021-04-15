28d73354366614ea6dc149c7f7a016f32c763d31
Eva Szatmari <szateva@gmail.com>
2021-04-15 10:20:04 +0100

    Session1Extra5 solved

--- Session1Extra5.py
+++ Session1Extra5.py
@@ -10,3 +10,9 @@ TESTS:
 Input: 466.345 Output: 3
 Input: 0.001 Output: 0
 Input: 45.0 Output: 0 """
+
+num = float(input("Type your number: "))
+fraction_part = num % 1
+times_ten = round(fraction_part * 10, 3)
+first_digit = int(times_ten // 1)
+print(first_digit)
